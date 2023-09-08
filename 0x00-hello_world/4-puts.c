dd7ccae674e4d9ecbc86ee92e8760cbe4d57f536
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-07 19:12:05 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-/** main - Entry point
+
+/**
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: A C program that prints with puts function
+ *
+ * Return: always 0 (Success)
 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
