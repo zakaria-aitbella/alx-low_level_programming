cba8d808acf33dc7fdd223794cb9e719c7a9b8f0
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-07 19:24:03 -0700

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progran that prints the size of  types on the computer
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
+
