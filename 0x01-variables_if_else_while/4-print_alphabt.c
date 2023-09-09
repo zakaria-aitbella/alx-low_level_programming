3d4438c1115cbc6888b91b3b9e658b9b0d847f04
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-08 19:37:33 -0700

    while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Print all the letters except q and e followed by a new line.
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+char lowercase = 'a';
+while (lowercase <= 'z')
+{
+if (lowercase == 'e' || lowercase == 'q')
+lowercase++;
+putchar(lowercase);
+lowercase++;
+}
+putchar('\n');
+return (0);
+}
+
