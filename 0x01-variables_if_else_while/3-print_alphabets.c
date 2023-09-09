c62e3e2a0020781372086a4e15935950413e6812
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-08 19:30:33 -0700

    while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, followed by a new line.
+ * main - prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line..
  *
  * Return: 0 (Success)
 */
