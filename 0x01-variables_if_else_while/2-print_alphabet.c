2dcac1d784a87b463710de01fffec6d3b6f180f8
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-08 19:06:02 -0700

    while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+char lowercase = 'a';
+while (lowercase =< 'z')
+{
+putchar(lowercase);
+}
+putchar(\n);
+return (0);
+}
+
