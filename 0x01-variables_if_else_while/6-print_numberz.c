37e8eeb237cc9209288990996e3ee4f47564c6ff
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-08 20:00:32 -0700

    while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+char num = '0';
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
+
