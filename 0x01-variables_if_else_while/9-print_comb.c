b6bd89b5b2023d5eb904e0813cee1cd81d8b0afc
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-10 22:57:01 -0700

    while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 
 /**
- * main -  a program that prints all possible combinations of single-digit numbers.
+ * main -  a program that prints all possible combinations
+ * of single-digit numbers.
  *
  * Return: 0 (Success)
 */
 int main(void)
 {
 int num;
-for(num = 0; num < 10; num++)
+for (num = 0; num < 10; num++)
 {
 putchar('0' + num);
 
