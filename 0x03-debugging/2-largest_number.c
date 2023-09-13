5679d0290867f5cda8633d533f0c12f573667faf
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-12 18:46:23 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,23 @@
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+int a, b, c;
+int largest;
+
+a = 972;
+b = -98;
+c = 0;
+largest = largest_number(a, b, c);
+
+printf("%d is the largest number\n", largest);
+
+return (0);
+}
 #include "main.h"
 
 /**
@@ -7,7 +27,6 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 int largest;
