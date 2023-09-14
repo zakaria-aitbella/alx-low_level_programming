2a02ff0ea480f6d2ee49aca2ebd26418f66de81e
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-14 14:54:50 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,7 +12,7 @@ int main(void) /* return 0 */
 char str[] = "_putchar";
 int ch;
 for (ch = 0; ch < 8; ch++)
-putchar(str[str]);
+putchar(str[ch]);
 _putchar('\n'); /* Newline character */
 return (0);
 }
