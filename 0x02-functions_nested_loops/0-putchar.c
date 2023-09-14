7b82736019b4a43eb370f60971655f962e00e228
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-14 13:39:18 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void) /* return 0 */
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n'); /* Newline character */
+return (0);
+}
+
