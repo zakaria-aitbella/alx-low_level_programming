c3077d8acf23ea771c35f46f400ddb83c9eae08a
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-15 19:16:33 -0700

    islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,12 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
+/**
+ * _islower - checks if a character is lowercase
+ * @c: The character to check
+ *
+ * Return: ,1 if c is a letter, lowercase or uppercase 0 otherwise
+*/
 int _isalpha(int c)
 {
 if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
