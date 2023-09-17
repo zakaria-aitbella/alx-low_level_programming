4cea1d871c2cb70cd53c755c5c26e8ca6ee0a869
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-16 21:14:08 -0700

    sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs -  computes the absolute value of an integer.
+*/
+int _abs(int)
+{
+	if (n < 0)
+	return -n;
+	else
+	return n;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,9 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
 
+
 int _islower(int c);
 int print_sign(int n);
+int _abs(int);
+
 #endif /* MAIN_H */
