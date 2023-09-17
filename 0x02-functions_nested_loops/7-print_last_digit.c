805a6b85e366bcd5a32f13c85995148066b471ff
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-16 21:42:06 -0700

    sign

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *  print_last_digit - prints the last digit of a number.
+ * @n : The character to check
+ * @lastDigit the output integer
+ * Return:the value of the last digit
+*/
+int print_last_digit(int lastDigit, n)
+	lastDigit = n % 10;
+	return (lastDigit);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,5 @@ int _isalpha(int c);
 int _islower(int c);
 int print_sign(int n);
 int _abs(int);
-
+int print_last_digit(int);
 #endif /* MAIN_H */
