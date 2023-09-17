5b57c4ebf3fe8033029dfcc83f6673a1e3b3556e
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-16 22:31:50 -0700

    sign

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * Jack Bauer, starting from 00:00 to 23:59.
+*/
+void jack_bauer(void)
+{
+int hr, int min;
+for (hr = 0; hr < 23; hr++)
+{
+	for (min = 0; min <= 59; min++)
+{
+ _putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
+_putchar(':');
+_putchar((min / 10) + 48);
+_putchar((min / 10) + 48);
+}
+}
+}
