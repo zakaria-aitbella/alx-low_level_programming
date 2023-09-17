926e4876589c7458057d6fe031505b15041b7223
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-16 22:46:08 -0700

    sign

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * Jack_bauer, starting from 00:00 to 23:59.
+ * jack_bauer - prints every minute of the day
+ * starting from 00:00 to 23:59.
 */
 void jack_bauer(void)
 {
