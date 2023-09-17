c7aabc3283571747257115397d479706d159a917
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-16 22:48:05 -0700

    sign

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ for (hr = 0; hr <= 23; hr++)
 	{
 for (min = 0; min <= 59; min++)
 		{
-_putchar((hr / 10) + 48);
-_putchar((hr % 10) + 48);
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
 _putchar(':');
-_putchar((min / 10) + 48);
-_putchar((min % 10) + 48);
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
 _putchar('\n');
 		}
 	}
