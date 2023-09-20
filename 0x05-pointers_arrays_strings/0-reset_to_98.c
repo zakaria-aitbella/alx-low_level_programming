3b2b753e9bb9e38fac5a18b4dd3f1bba912537da
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-19 19:02:05 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,9 @@
 /**
  * reset_to_98 -  takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n - the integer to updates the value it points to to 98
+ * @n: the integer to updates the value it points to to 98
 */
 
 void reset_to_98(int *n)
-	 n = 98;
+	*n = 98;
 }
