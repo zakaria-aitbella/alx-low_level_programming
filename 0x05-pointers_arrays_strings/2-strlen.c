a599c1362aef0ef2a0a4da0ac9e07e55ba3a9ac9
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-19 21:42:02 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - printd the length of the string.
+ * @s: the string to calculate.
+ * @len: the integer where to print the result.
+ * */
+
+int _strlen(char *s)
+{
+	int len= 0;
+		while (*s != '\0')
+	{
+	len++;
+	s++;
+	}
+return (len);
+}
