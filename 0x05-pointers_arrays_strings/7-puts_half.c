db4868acd2b1af54fe15a998c8d2bb4eaf2d30bd
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-20 20:08:24 -0700

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,14 @@
 */
 
 void puts_half(char *str)
-{int i;
+{
+	int i;
 for (i = 0; str[i] != '\0'; i++)
 ;
 i++;
 for (i /= 2; str[i] != '\0'; i++)
 {
-    putchar(str[i]);
+	putchar(str[i]);
 }
 putchar('\n');
 }
