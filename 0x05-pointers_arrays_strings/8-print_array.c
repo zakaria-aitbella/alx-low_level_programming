c74ec1c09b1cbda24f49e0a0287d3c67704725f7
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-20 22:31:17 -0700

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-for (i = 0; i <= n i++)
+for (i = 0; i <= n; i++)
 {
-printf("%d", a[n]);
-if (n < 4)
+printf("%d", a[i]);
+if (i < 4)
 {
-printf(" ,");
+printf(", ");
 }
 }
 }
