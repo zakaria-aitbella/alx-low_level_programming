ccd78ea58b7071b7ba0cf65fd79870468b77f447
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-20 20:07:03 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the seconf half of the string
+ *
+ * @str: char array string type.
+ *
+ * Description: if odd number og chars, print (length - 1) / 2
+*/
+
+void puts_half(char *str)
+{int i;
+for (i = 0; str[i] != '\0'; i++)
+;
+i++;
+for (i /= 2; str[i] != '\0'; i++)
+{
+    putchar(str[i]);
+}
+putchar('\n');
+}
