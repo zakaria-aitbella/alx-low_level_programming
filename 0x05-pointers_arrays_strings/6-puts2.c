ef6f012e5f167b73d1e4a37ffb373691448ee5e5
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-09-20 19:47:36 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - pritd every character of a string.
+ *
+ * @str: string parameter input.
+ *
+ * Return: Nothing.
+*/
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] % 2 == 0)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
