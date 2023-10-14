54ff177741ad885abbb68e29284434e1ca5fb56d
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-10-13 21:55:13 -0700

    recursions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,16 +5,30 @@
  * @n: int
  * Return: int
 */
+int check_prime(int n, int othrn);
 int is_prime_number(int n)
 {
+return (check_prime(n, 2));
+}
 
-if (n < 1 || n / n == 1)
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: int
+ * Return: int
+*/
+
+int check_prime(int n, int othrn)
+{
+
+if (othrn >= n && n > 1)
 {
 return (1);
 }
-else if (n / n < 1)
+else if (n % othrn == 0 || n <= 1)
 {
 return (0);
 }
-	return (0);
+else
+return (check_prime(n, othrn + 1));
 }
