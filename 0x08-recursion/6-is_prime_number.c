655337d63fb96b35afcaf4ad6a927964a6093a08
zakaria-aitbella <aitbellazakaria592@gmail.com>
2023-10-13 21:47:09 -0700

    recursions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@
 int is_prime_number(int n)
 {
 
-if (n / n == 1)
+if (n < 1 || n / n == 1)
 {
 return (1);
 }
@@ -16,8 +16,5 @@ else if (n / n < 1)
 {
 return (0);
 }
-else if (n <= 1)
-{
 	return (0);
-}
 }
